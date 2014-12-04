f18cfe485c016fd07167308e8197b0cb0a9976f0
aric87 <aric87@comcast.net>
2014-12-04 13:10:00 -0500

    styles, responsiveness

--- shopping.js
+++ shopping.js
@@ -1,39 +1,30 @@
-
 $(document).ready(function() {
-
 //enter will click add
-$('.I').keyup(function(event){
+	$('.I').keyup(function(event) {
 		if(event.keyCode == 13) {
 			event.preventDefault();
 			$('.add').click();
 		};
 	});	
-
-//add item
-$('button.add').click(function() {
-if ( $('.I').val().trim().length == 0 ) {
-	$("#error").show();	  
-} else { $('ul').append(
-	'<li class="item">' + '<p>' + $('.I').val() 
-	+ '</p>' + '<i class="fa fa-trash "></i></li>'    );
-	  }
-
-						});
-
-//add item:
-$('ul').on('click', 'li', function(){
-	
-	event.preventDefault;
-	$(this).toggleClass('strike'); 
-	$(this).find('i').toggleClass('fa-trash').toggleClass('fa-trash-o');
+	//add item
+	$('button.add').click(function() {
+		if ($('.I').val().trim().length == 0 ) {
+			$("#error").show();	  
+		} else { 
+			$('ul').append('<li class="item">' + '<p> ' + $('.I').val() + '</p>' + '<i class="fa fa-trash "></i></li>');
+	 	}
 	});
-
-//remove item:
-$('ul').on('click', 'i', function(event){
-	event.preventDefault;
-	$(this).closest('li').remove();
+	//add item:
+	$('ul').on('click', 'li', function(event) {	
+		event.preventDefault;
+		$(this).toggleClass('strike'); 
+		$(this).find('i').toggleClass('fa-trash').toggleClass('fa-trash-o');
 	});
-
+	//remove item:
+	$('ul').on('click', 'i', function(event) {
+		event.preventDefault;
+		$(this).closest('li').remove();
+	});
 });
 
 
